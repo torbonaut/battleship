1ef8dfeac03f61b86b42dfd3e9b2e871a37a0874
Torben Gallob <torben.gallob@gmail.com>
2024-06-23 23:00:10 +0200

    added routes for google auth api init

---
+++ src/routes.tsx
@@ -0,0 +1,14 @@
+export const routes = [
+  {
+    path: "/",
+    element: <p>Home</p>,
+  },
+  {
+    path: "/gdpr",
+    element: <p>GDPR</p>,
+  },
+  {
+    path: "/terms",
+    element: <p>Terms</p>,
+  },
+];
